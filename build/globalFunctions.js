b0e7ce847f97781a372844d7fbca74f515dc111d
Robert Gudat <robert.gudat@web.de>
2023-02-08 04:29:21 +0100

    Removed old files.

--- build/globalFunctions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ArrayBufferDecoder = void 0;
-const util_1 = require("util");
-exports.ArrayBufferDecoder = new util_1.TextDecoder('utf-8');
