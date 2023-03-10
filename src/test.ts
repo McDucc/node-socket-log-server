af7cc1f39c22d99f840783b3c656e62fbbf081bd
Robert Gudat <robert.gudat@web.de>
2023-03-10 17:23:44 +0100

    Changed tsconfig to fit top level await and removed windows from build targets.

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
 import test from 'node:test';
+import SetupPostgresPool from './database/PostgresSetup';
 import LogService from './services/LogService';
+
 
 async function wait(ms: number) {
     await new Promise((resolve) => {
@@ -7,14 +9,24 @@ async function wait(ms: number) {
     });
 }
 
-test('test 1', async (t) => {
-    let logService = new LogService();
-    await wait(3000);
-    await fetch('http://' + logService.address);
-});
+async function testSuite() {
 
-test('Test 2', async (t) => {
-    await t.test('test 2.1');
-    await t.test('test 2.2');
-    await t.test('test 2.3');
-});
+    let pool = await SetupPostgresPool(1);
+
+    await pool.initialize();
+
+    test('test 1', async (t) => {
+        let logService = new LogService(pool);
+        await wait(3000);
+        await fetch('http://' + logService.address);
+    });
+
+    test('Test 2', async (t) => {
+        await t.test('test 2.1');
+        await t.test('test 2.2');
+        await t.test('test 2.3');
+    });
+
+}
+
+await testSuite();
